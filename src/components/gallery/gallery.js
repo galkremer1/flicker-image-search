4f3ff5e0170a5b320780c70da4653bb36ae55e2f
galkremer1 <galkremer1@walla.co.il>
2019-03-23 13:14:28 +0200

    added a full sized gallery component

--- src/components/gallery/gallery.js
+++ src/components/gallery/gallery.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import LightboxGallery from './lightBoxGallery';
 import { withStyles } from '@material-ui/core/styles';
 import { styles } from './galleryStyle';
 
-
 class Gallery extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isGalleryOpen: false
+    };
+  }
 
   componentDidMount () {
     window.addEventListener('scroll', this.handleScroll);
@@ -23,17 +30,34 @@ handleScroll = (event) => {
 
 }
 
-  render() {
-    const { classes, photos, isLoading, error} = this.props;
-    return (
-      <div className={classes.galleryContainer}>
-        {photos.map((photo)=>{
-          return  <img className={classes.photo} key={photo.id} alt={photo.title} src={photo.thumbnailUrl}/ >
-        })}
-        {!isLoading && !error && photos.length === 0 && <div>
-          No results
-        </div>}
-      </div>
+closeGallery = () =>{
+  this.setState({
+    isGalleryOpen: false,
+  })
+}
+
+openGallery = (photo,index) => {
+  console.log(photo, index);
+  this.setState({
+    isGalleryOpen: true,
+    galleryPhotoIndex: index
+  })
+}
+
+render() {
+  const { classes, photos, isLoading, error} = this.props;
+  const { isGalleryOpen, galleryPhotoIndex } = this.state;
+  return (
+    <div className={classes.galleryContainer}>
+      {photos.map((photo, i)=>{
+        return  <img className={classes.photo} key={photo.id} onClick={()=>{this.openGallery(photo, i)}}
+                      alt={photo.title} src={photo.thumbnailUrl}/ >
+      })}
+      {!isLoading && !error && photos.length === 0 && <div>
+        No results
+      </div>}
+      <LightboxGallery images={photos} isGalleryOpen={isGalleryOpen} galleryPhotoIndex={galleryPhotoIndex} closeGallery={this.closeGallery}/>
+    </div>
     );
   }
 }
---
+++ src/components/gallery/lightBoxGallery.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import Lightbox from 'react-image-lightbox';
+import 'react-image-lightbox/style.css'; 
+
+export default class LightboxGallery extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      photoIndex: null
+    };
+  }
+
+  closeGallery = () => {
+    const { closeGallery } = this.props;
+    this.setState({
+      photoIndex: null
+    }); 
+    closeGallery();
+  }
+
+  render() {
+    const { images, isGalleryOpen, galleryPhotoIndex} = this.props;
+    let {photoIndex} = this.state;
+    photoIndex = photoIndex || galleryPhotoIndex;
+    return (
+      <div>
+        {isGalleryOpen && (
+          <Lightbox
+            imageTitle={images[photoIndex].title}
+            mainSrc={images[photoIndex].fullImageUrl}
+            nextSrc={images[(photoIndex + 1) % images.length].fullImageUrl}
+            prevSrc={images[(photoIndex + images.length - 1) % images.length].fullImageUrl}
+            onCloseRequest={this.closeGallery}
+            onMovePrevRequest={() =>
+              this.setState({
+                photoIndex: (photoIndex + images.length - 1) % images.length
+              })
+            }
+            onMoveNextRequest={() =>
+              this.setState({
+                photoIndex: (photoIndex + 1) % images.length
+              })
+            }
+          />
+        )}
+      </div>
+    );
+  }
+}
