6e3d924c64fa25d81b005846e0ab231cf9a5849f
galkremer1 <galkremer1@walla.co.il>
2019-03-25 12:45:51 +0200

    Updates

--- precache-manifest.b26d0f0c3048f22ee6dec6703a3b748d.js
+++ precache-manifest.b26d0f0c3048f22ee6dec6703a3b748d.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/flicker-image-search/static/js/runtime~main.75c3c9e2.js"
   },
   {
-    "revision": "18f6779115efd3203b3b",
-    "url": "/flicker-image-search/static/js/main.b9e5c669.chunk.js"
+    "revision": "2587c2224255a7c67c61",
+    "url": "/flicker-image-search/static/js/main.7a330b6c.chunk.js"
   },
   {
     "revision": "4cdf54dd1bc3027fec51",
     "url": "/flicker-image-search/static/js/2.2ee7efb5.chunk.js"
   },
   {
-    "revision": "18f6779115efd3203b3b",
+    "revision": "2587c2224255a7c67c61",
     "url": "/flicker-image-search/static/css/main.7e0276d4.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/flicker-image-search/static/css/2.385ad5d1.chunk.css"
   },
   {
-    "revision": "da2628e9ced72482c78a3973efa30167",
+    "revision": "56e8f9b34117b88633122d1f495cec1a",
     "url": "/flicker-image-search/index.html"
   }
 ];
